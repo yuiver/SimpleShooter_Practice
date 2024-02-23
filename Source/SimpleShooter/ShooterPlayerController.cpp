dd62e6a0c7555ef956968a050f455a4659676d11
youmin <dbals1523@gmail.com>
2024-02-23 12:15:45 +0900

    [Add] Added death haptic feedback

--- Source/SimpleShooter/ShooterPlayerController.cpp
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -3,10 +3,13 @@
 
 #include "ShooterPlayerController.h"
 #include "TimerManager.h"
+#include "BhapticsSDK2.h"
 
 void AShooterPlayerController::GameHasEnded(class AActor* EndGameFocus, bool bIsWinner)
 {
 	Super::GameHasEnded(EndGameFocus, bIsWinner);
+
+	UBhapticsSDK2::PlayHaptic("died");
 
 	UE_LOG(LogTemp, Warning, TEXT("We Have Finished."));
 
